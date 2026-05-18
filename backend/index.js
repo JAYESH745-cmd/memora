d63c33e46efef3ebd5883bb7daa3207b199d8ec9
Jayesh Arora <jayesharora0001@gmail.com>
2026-05-18 15:45:25 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -20,15 +20,14 @@ const __dirname =path.dirname(__filename);
 connectDB();
 const app=express();
 app.use(
-  cors({
-    origin: "https://memora-brown-seven.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
+    cors({
+        origin:"*",
+        methods:["GET","POST","PUT","DELETE"],
+        allowedHeaders:["Content-Type","Authorization"],
+        credentials:true,
+    })
 );
 
-app.options("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({extended :true}));
 
